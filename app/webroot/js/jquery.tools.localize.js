0241b895d3b7e926c29ef8acdefca7a86d811a40
Julio César Domínguez Giraldo <jucedogi@ubuntu-vb.(none)>
2012-05-25 14:37:59 -0500

    localizar validator

---
+++ app/webroot/js/jquery.tools.localize.js
@@ -0,0 +1,14 @@
+$(function() {
+	$.tools.validator.localize(
+		"es", {
+			'*':'Corrija el valor en este campo',
+			':email':'Ingrese un correo electrónico válido',
+			':number':'Ingrese un valor numérico',
+			':radio':'Seleccione una opción',
+			':url':'Ingrese una URL válida',
+			'[max]':'Ingrese un valor no mayor a $1',
+			'[min]':'Ingrese un valor mayor o igual a $1',
+			'[required]':'Este campo es requerido'
+		}
+	);
+});
