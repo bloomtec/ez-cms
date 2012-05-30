6380521bf28026f3f25f54f1823b29c026cc5e94
Julio César Domínguez Giraldo <jucedogi@ubuntu-vb.(none)>
2012-05-30 07:32:37 -0500

    revs

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -30,6 +30,11 @@ App::uses('AppController', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers/pages-controller.html
  */
 class PagesController extends AppController {
+	
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this -> Auth -> allow('display', 'home', 'construccion');
+	}
 
 	/**
 	 * Controller name
@@ -82,8 +87,9 @@ class PagesController extends AppController {
 	}
 
 	public function home() {
-		
+
 	}
+
 	public function construccion() {
 		$this -> layout = 'ajax';
 	}
