dba1ceb2821c3a19080d78e71ef99f47e92f2dd9
Julio César Domínguez Giraldo <jucedogi@ubuntu-vb.(none)>
2012-06-14 18:03:36 -0500

    revs carrito

--- app/Plugin/BCart/Controller/ShoppingCartsController.php
+++ app/Plugin/BCart/Controller/ShoppingCartsController.php
@@ -8,6 +8,8 @@ class ShoppingCartsController extends BCartAppController {
 
 	public function beforeFilter() {
 		parent::beforeFilter();
+		$this -> ShoppingCart -> Behaviors -> attach('Containable');
+		$this -> ShoppingCart -> contain('CartItem', 'CartItem.Product', 'CartItem.ProductSize');
 		$this -> Auth -> Allow('get', 'addCartItem', 'removeCartItem', 'updateCartItem');
 	}
 	
@@ -17,9 +19,8 @@ class ShoppingCartsController extends BCartAppController {
 	 * @return Información del carrito
 	 */
 	public function get() {
-		$this -> autoRender = false;
 		Configure::write('debug', 0);
-		$user_id = $this -> getUserId();
+		$user_id = $this -> getUserId(); 
 		if($user_id) {
 			/** hay usuario logueado **/
 			$shopping_cart = $this -> ShoppingCart -> findByUserId($user_id);
@@ -36,7 +37,6 @@ class ShoppingCartsController extends BCartAppController {
 			}
 		} else {
 			/** no hay usuario logueado **/
-			//session_set_cookie_params(3600, '/', 'localhost', false, false);
 			$shopping_cart = $this -> readCookie();
 			if(empty($shopping_cart)) {
 				$shopping_cart = array(
@@ -57,10 +57,13 @@ class ShoppingCartsController extends BCartAppController {
 			/** llegó la info proceder a guardar **/
 			$user_id = $this -> getUserId();
 			if($user_id) {
+				echo 'hay usuario<br /><br />';
 				/** tratar de obtener el carrito de la BD y verificar si existe **/
-				$shopping_cart = $this -> ShoppingCart -> findByUserId($user_id);
+				$shopping_cart = $this -> get();
+				print_r($shopping_cart); echo '<br /><br />';
 				if($shopping_cart) {
 					/** carrito existe, agregar el ítem **/
+					echo 'hay carrito, crear el item de carrito<br /><br />';
 					$cart_item = array(
 						'shopping_cart_id' => $shopping_cart['ShoppingCart']['id'],
 						'product_id' => $product_id,
@@ -69,8 +72,10 @@ class ShoppingCartsController extends BCartAppController {
 					);
 					$this -> ShoppingCart -> CartItem -> create();
 					if($this -> ShoppingCart -> CartItem -> save($cart_item)) {
+						echo 'se creó el item de carrito<br /><br />';
 						$shopping_cart['success'] = true;
 					} else {
+						echo 'no se creó el item de carrito<br /><br />';
 						$shopping_cart['success'] = false;
 					}
 					echo json_encode($shopping_cart);
@@ -85,17 +90,14 @@ class ShoppingCartsController extends BCartAppController {
 				$this -> ShoppingCart -> CartItem -> ProductSize -> recursive = -1;
 				$product_size = $this -> ShoppingCart -> CartItem -> ProductSize -> read(null, $product_size_id);
 				$cart_item = array(
-					'CartItem' => array(
-						'id' => count($shopping_cart['ShoppingCart']['CartItem']) + 1,
-						'product_id' => $product_id,
-						'product_size_id' => $product_size_id,
-						'quantity' => $quantity
-					),
+					'id' => count($shopping_cart['ShoppingCart']['CartItem']) + 1,
+					'product_id' => $product_id,
+					'product_size_id' => $product_size_id,
+					'quantity' => $quantity,
 					'Product' => $product['Product'],
 					'ProductSize' => $product_size['ProductSize']
 				);
 				$shopping_cart['ShoppingCart']['CartItem'][] = $cart_item;
-				//echo json_encode($cart_item);
 				$this -> writeCookie($shopping_cart);
 				$shopping_cart = $this -> get();
 				$shopping_cart['success'] = true;
@@ -136,7 +138,7 @@ class ShoppingCartsController extends BCartAppController {
 				$shopping_cart = $this -> get();
 				$item_deleted = false;
 				foreach($shopping_cart['ShoppingCart']['CartItem'] as $key => $cart_item) {
-					if($cart_item['CartItem']['id'] == $cart_item_id) {
+					if($cart_item['id'] == $cart_item_id) {
 						unset($shopping_cart['ShoppingCart']['CartItem'][$key]);
 						$item_deleted = true;
 					}
@@ -183,8 +185,8 @@ class ShoppingCartsController extends BCartAppController {
 				$shopping_cart = $this -> get();
 				$item_modified = false;
 				foreach($shopping_cart['ShoppingCart']['CartItem'] as $key => $cart_item) {
-					if($cart_item['CartItem']['id'] == $cart_item_id) {
-						$shopping_cart['ShoppingCart']['CartItem'][$key]['CartItem']['quantity'] = $quantity;
+					if($cart_item['id'] == $cart_item_id) {
+						$shopping_cart['ShoppingCart']['CartItem'][$key]['quantity'] = $quantity;
 						$item_modified = true;
 					}
 				}
