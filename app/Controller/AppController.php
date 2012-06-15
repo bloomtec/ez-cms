3e09d9452c3996a9264cff1bc241793a96e544ac
Julio César Domínguez Giraldo <jucedogi@ubuntu-vb.(none)>
2012-06-15 11:31:47 -0500

    revs carrito

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -32,44 +32,32 @@ App::uses('Controller', 'Controller');
  * @link http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
-	
-	public $cacheAction = true;
 
-	public $components = array(
-		'Auth',
-		'Acl',
-		'Session',
-		'Cookie'
-	);
+	public $cacheAction = true;
 	
+	private $identifier = '';
+
+	public $components = array('Auth', 'Acl', 'Session', 'Cookie');
+
 	public function beforeFilter() {
 		$this -> beforeFilterAuthConfig();
 		$this -> beforeFilterCookieConfig();
+		$this -> setIdentifier();
 		if (isset($this -> params["prefix"]) && $this -> params["prefix"] == "admin") {
-			$this -> layout="Ez.default";
+			$this -> layout = "Ez.default";
 		}
-		
 		// Verificación ACL
 		$this -> aclVerification();
 	}
-	
+
 	private function beforeFilterAuthConfig() {
-		$this -> Auth -> authorize = array(
-			'Actions' => array(
-				'actionPath' => 'controllers'
-			)
-		);
-		$this -> Auth -> loginAction = array(
-			'controller' => 'users',
-			'action' => 'login',
-			'plugin' => 'user_control',
-			'admin' => false
-		);
+		$this -> Auth -> authorize = array('Actions' => array('actionPath' => 'controllers'));
+		$this -> Auth -> loginAction = array('controller' => 'users', 'action' => 'login', 'plugin' => 'user_control', 'admin' => false);
 		$this -> Auth -> authError = __('No tiene permiso para ver esta sección', true);
 		$this -> Auth -> loginRedirect = array('plugin' => 'user_control', 'controller' => 'users', 'action' => 'profile');
 		$this -> Auth -> logoutRedirect = array('plugin' => 'user_control', 'controller' => 'users', 'action' => 'login');
 	}
-	
+
 	private function beforeFilterCookieConfig() {
 		if(isset($this -> Cookie -> name) && !empty($this -> Cookie -> name)) {
 			$this -> Cookie -> name = 'PriceShoes';
@@ -94,16 +82,33 @@ class AppController extends Controller {
 		}
 	}
 	
+	private function setIdentifier() {
+		if(!$this -> Cookie -> read('User.identifier')) {
+			$this -> loadModel('BCart.ShoppingCart');
+			$identifier = rand(100000000000, 999999999999);
+			if(!$this -> ShoppingCart -> findByIdentifier($identifier)) {
+				$this -> Cookie -> write('User.identifier', $identifier);
+			} else {
+				$this -> setIdentifier();
+			}
+		}
+		$this -> identifier = $this -> Cookie -> read('User.identifier');
+	}
+	
+	public function getIdentifier() {
+		return $this -> identifier;
+	}
+
 	/**
 	 * Vericar el acceso de un usuario a una función mediante ACL
-	 * 
+	 *
 	 * @param string $username El nombre de usuario
 	 * @param string $controlador Nombre del controlador; i.e.: Users
 	 * @param string $accion Nombre de la función
-	 * @return true o false acorde si hay o no acceso 
+	 * @return true o false acorde si hay o no acceso
 	 */
 	protected function verifyUserAccess($username, $controlador = null, $accion = null) {
-		if(!$controlador || !$accion) {
+		if (!$controlador || !$accion) {
 			// Armar la ruta
 			$ruta = '';
 			for ($i = 0; $i < count($this -> params['ruta']); $i++) {
@@ -113,44 +118,44 @@ class AppController extends Controller {
 				}
 			}
 			return $this -> Acl -> check($username, $ruta);
-		} elseif(!$accion) {
+		} elseif (!$accion) {
 			return $this -> Acl -> check($username, $controlador);
 		} else {
 			return $this -> Acl -> check($username, $controlador . '/' . $accion);
 		}
 	}
-	
+
 	/**
 	 * Cuadrar accesos mediante ACL
-	 * 
+	 *
 	 * @return void
 	 */
 	protected function aclVerification() {
-		if($this -> Auth -> user()) {
+		if ($this -> Auth -> user()) {
 			$this -> loadModel('Role');
 			$roles = $this -> Role -> find('all');
 			foreach ($roles as $key => $role) {
-				if($role['Role']['role'] != 'Administrador') {
-						
+				if ($role['Role']['role'] != 'Administrador') {
+
 					// Permitir acceso total en ciertos controladores inicialmente si no se es admin
 					$this -> Acl -> allow($role['Role']['role'], $this -> name);
-					
+
 					// Negar acceso a los siguientes métodos administrativos
-					foreach($this -> methods as $key => $method) {
-						if((!strstr($method, 'admin_')) && (!strstr($method, 'aclVerification'))  && (!strstr($method, 'verifyUserAccess'))) {
-							if(!$this -> Acl -> check($role['Role']['role'], $this -> name . '/' . $method)) {
+					foreach ($this -> methods as $key => $method) {
+						if ((!strstr($method, 'admin_')) && (!strstr($method, 'aclVerification')) && (!strstr($method, 'verifyUserAccess'))) {
+							if (!$this -> Acl -> check($role['Role']['role'], $this -> name . '/' . $method)) {
 								$this -> Acl -> deny($role['Role']['role'], $this -> name . '/' . $method);
 							}
-						} elseif(strstr($method, 'admin_')) {
-							if($this -> Acl -> check($role['Role']['role'], $this -> name . '/' . $method)) {
+						} elseif (strstr($method, 'admin_')) {
+							if ($this -> Acl -> check($role['Role']['role'], $this -> name . '/' . $method)) {
 								$this -> Acl -> deny($role['Role']['role'], $this -> name . '/' . $method);
 							}
 						}
 					}
-					
+
 				}
 			}
 		}
 	}
-	
+
 }
