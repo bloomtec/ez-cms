b58bc8f433b5fc2319d10672da0c4a08a37e55bd
Julio César Domínguez Giraldo <juliodominguez@bloomweb.co>
2013-04-17 15:17:20 -0500

    camrevs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # INITIAL CONFIGURATION
-set :application, "priceshoes.bloomweb.co"
+set :application, "priceshoes.com.co"
 set :export, :remote_cache
 set :keep_releases, 5
 set :cakephp_app_path, "app"
@@ -7,16 +7,16 @@ set :cakephp_core_path, "cake"
 #default_run_options[:pty] = true # Para pedir la contraseña de la llave publica de github via consola, sino sale error de llave publica.
 
 # DEPLOYMENT DIRECTORY STRUCTURE
-set :deploy_to, "/home/embalao/priceshoes.bloomweb.co"
+set :deploy_to, "/home/prices10/public_html"
 
 # USER & PASSWORD
-set :user, 'embalao'
-set :password, 'rr40r900343'
+set :user, 'prices10'
+set :password, '4ALLmR2N4XJR'
 
 # ROLES
-role :app, "priceshoes.bloomweb.co"
-role :web, "priceshoes.bloomweb.co"
-role :db, "priceshoes.bloomweb.co", :primary => true
+role :app, "priceshoes.com.co"
+role :web, "priceshoes.com.co"
+role :db, "priceshoes.com.co", :primary => true
 
 # VERSION TRACKER INFORMATION
 set :scm, :git
@@ -32,8 +32,8 @@ namespace :deploy do
   task :stop do ; end
   
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "cp /home/embalao/priceshoes.bloomweb.co/current/. /home/embalao/priceshoes.bloomweb.co/ -R"
-    run "chmod 777 /home/embalao/priceshoes.bloomweb.co/app/tmp/ -R"
+    run "cp /home/prices10/public_html/current/. /home/prices10/public_html/ -R"
+    run "chmod 777 /home/prices10/public_html/app/tmp/ -R"
   end
   
 end
